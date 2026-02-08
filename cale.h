d51363d219692ab9591261788ddd57789abede36
BruceWag <tonnamerak@gmail.com>
2026-02-08 23:47:37 +0700

    add cale_HHHH

---
+++ cale.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+void displayTitle();
+void displayMenu();
+double calculate(double a, double b, char op);
+void getInput(double a, double b, char *op);
+
+#endif
