00900631c89b6954374410f9eb96389a16643891
Topthai <top994.top@gmail.com>
2026-02-08 23:20:05 +0700

    add display

---
+++ display.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "calculator.h"
+
+void displayTitle() {
+    printf("=================================\n");
+    printf("        SIMPLE CALCULATOR\n");
+    printf("=================================\n");
+}
+
+void displayMenu() {
+    printf("Operators: +  -  *  /  %%\n");
+}
